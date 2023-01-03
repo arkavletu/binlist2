f67e3e6b38b71583d2353956c5e94eb224fe4b32
arkavletu <whitestring@inbox.ru>
2023-01-04 00:06:43 +0300

    pojo

--- app/src/main/java/com/example/binlistapp/dto/CardInfo.kt
+++ app/src/main/java/com/example/binlistapp/dto/CardInfo.kt
@@ -1,14 +1,29 @@
 package com.example.binlistapp.dto
 
-data class CardInfo (
-    val length: Int,
-    val luhn: Boolean,
-    val scheme: String,
-    val type: String,
-    val brand: String,
-    val prepaid: Boolean,
-    val bank: String,
-    val country: String,
-    val id: Long = 0,
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 
-    )
+class CardInfo(
+    @SerializedName("number")
+    @Expose
+    val number: Number,
+    @SerializedName("scheme")
+    @Expose
+    val scheme: String,
+    @SerializedName("type")
+    @Expose
+    val type: String,
+    @SerializedName("brand")
+    @Expose
+    val brand: String,
+    @SerializedName("prepaid")
+    @Expose
+    val prepaid: Boolean,
+    @SerializedName("country")
+    @Expose
+    val country: Country,
+    @SerializedName("bank")
+    @Expose
+    val bank: Bank,
+    val id: Long = 0L
+)
